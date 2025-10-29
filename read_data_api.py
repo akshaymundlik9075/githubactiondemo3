e9c80620041f4323bf2cee7129087c9dffe3d8f8
Akshay Mundlik <limitededition4597@gmail.com>
2025-10-29 22:02:11 +0530

    testing3

--- read_data_api.py
+++ read_data_api.py
@@ -2,13 +2,13 @@ import requests
 import pandas as pd
 import os
 
-token=os.getenv{"API_TOKEN"}
+token=os.getenv("API_TOKEN")
 print(f"Token : {token}")
 if token=="12345abcde":
     print("correct")
 else:
     print("Incorrect")
-    
+
 #response=requests.get("https://jsonplaceholder.typicode.com/users")
 #data=response.json()
 #df=pd.DataFrame(data)
